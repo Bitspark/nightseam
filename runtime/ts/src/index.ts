d26b618fb3dbf99a97843352e9d659a2dda84f17
Julian Matschinske <julian.matschinske@bitspark.com>
2026-09-21 05:02:09 +0200

    runtime/ts: export canonical declaration composition helpers.

--- runtime/ts/src/index.ts
+++ runtime/ts/src/index.ts
@@ -19,6 +19,7 @@ export type { Trace, Propagator } from './trace.ts';
 export { webSocketConnection } from '@nightseam/duplex';
 export type { Frame, ConnectionState, ConnectionHandlers, FrameConnection, WebSocketLike } from '@nightseam/duplex';
 export { createValidator } from './validate.ts';
+export { withDeclaration, boundDeclaration, declarationDigest, typeDeclaration } from './declaration_identity.ts';
 export { IDENTITY_METHOD, checkIdentity, identityHandler } from './identity.ts';
 export type { DeclarationIdentity } from './identity.ts';
 export { scalarJSON as validateUnicodeJSON } from './unicode.ts';
